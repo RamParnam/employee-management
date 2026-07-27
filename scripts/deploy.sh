a7ebf2fb02129f79bf4ef0b1945a403d07d7df7f
Ram Parnam <ramu.parnam@gmail.com>
2026-07-27 18:04:14 +0530

    added delete old container step in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,39 @@
 
 set -e
 
-if [ $# -ne 2 ]; then
-    echo "Usage: ./deploy.sh <image-name> <container-name>"
+if [ $# -ne 1 ]; then
+    echo "Usage: ./deploy.sh <image-name>"
     exit 1
 fi
 
 IMAGE_NAME="$1"
-CONTAINER_NAME="$2"
+
+
+echo "Reading active port from Nginx..."
+
+ACTIVE_PORT=$(grep "proxy_pass" /etc/nginx/sites-available/default | grep -oE '[0-9]+' | tail -1)
+
+echo "Current Active Port : $ACTIVE_PORT"
+
+if [ "$ACTIVE_PORT" = "5000" ]; then
+    NEW_PORT=5001
+
+    ACTIVE_CONTAINER="employee-management-blue"
+
+    NEW_CONTAINER="employee-management-green"
+
+else
+    NEW_PORT=5000
+
+    ACTIVE_CONTAINER="employee-management-green"
+
+    NEW_CONTAINER="employee-management-blue"
+
+fi
+
+echo "New Deployment Port : $NEW_PORT"
+echo "Current Container   : $ACTIVE_CONTAINER"
+echo "New Container       : $NEW_CONTAINER"
 
 echo "=============================="
 echo "Starting Deployment..."
@@ -17,16 +43,22 @@ echo "=============================="
 echo "Pulling latest Docker image..."
 docker pull "$IMAGE_NAME"
 
-echo "Stopping existing container..."
-docker stop "$CONTAINER_NAME" || true
+echo "Removing previous $NEW_CONTAINER (if exists)..."
 
-echo "Removing existing container..."
-docker rm "$CONTAINER_NAME" || true
+docker stop "$NEW_CONTAINER" || true
+docker rm "$NEW_CONTAINER" || true
+
+
+#echo "Stopping existing container..."
+#docker stop "$CONTAINER_NAME" || true
+
+#echo "Removing existing container..."
+#docker rm "$CONTAINER_NAME" || true
 
 echo "Starting new container..."
 docker run -d \
-  --name "$CONTAINER_NAME" \
-  -p 5000:5000 \
+  --name "$NEW_CONTAINER" \
+  -p "$NEW_PORT":5000 \
   "$IMAGE_NAME"
 
 echo "Waiting for application to start..."
@@ -34,7 +66,7 @@ sleep 10
 
 echo "Checking application health..."
 
-curl --fail http://localhost:5000/health
+curl --fail http://localhost:"$NEW_PORT"/health
 
 echo ""
 echo "================================="
